180a3236b0d8ae5bb2b4a37b05065d4378828d1a
Remigiusz Wojak <w65577@student.wsiz.edu.pl>
2022-10-20 12:55:46 +0200

    first  commit

---
+++ Services/Calculate.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab1.Services
+{
+    public class Calculate
+    {
+        public Calculate(int value)
+        {
+            Value = value;
+        }
+
+        private int Value { get; set; }
+
+        public void Add(int value)
+        {
+            Value += value;
+        }
+
+        public void NotAdd(int value)
+        {
+            Value -= value;
+        }
+
+        public int GetValue()
+        {
+            return Value;
+        }
+
+    }
+}
